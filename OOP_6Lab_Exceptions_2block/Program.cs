6af2f64939b4d6b4d40b5bf67e0ed7c59badb9e5
romkaCHNU <chub.roman1124@vu.cdu.edu.ua>
2025-12-04 10:36:03 +0200

    Add 2 block of work, not fully tested

--- OOP_6Lab_Exceptions_2block/Program.cs
+++ OOP_6Lab_Exceptions_2block/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace OOP_6Lab_Exceptions_2block
+﻿using System.Text.RegularExpressions;
+
+namespace OOP_6Lab_Exceptions_2block
 {
     internal class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
+            String folder = Directory.GetCurrentDirectory();
 
+            Regex regexExtForImage = new Regex("^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$",
+                RegexOptions.IgnoreCase);
+
+            String[] files = Directory.GetFiles(folder);
+
+            foreach (String fileName in files)
+            {
+                String ext = Path.GetExtension(fileName).TrimStart('.');
+
+                try
+                {
+                    Bitmap image = new Bitmap(fileName);
+
+                    image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+                    String nameOnly = Path.GetFileNameWithoutExtension(fileName);
+                    String newFile = Path.Combine(folder, nameOnly + "-mirrored.gif");
+
+                    image.Save(newFile, System.Drawing.Imaging.ImageFormat.Gif);
+
+                    image.Dispose();
+                }
+                catch
+                {
+                    if (regexExtForImage.IsMatch(ext))
+                    {
+                        MessageBox.Show(
+                            "Файл \"" + fileName + "\" має графічне розширення,\n" +
+                            "але не містить картинки.",
+                            "Помилка читання",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning
+                        );
+                    }
+                }
+            }
+
+            MessageBox.Show("Обробку завершено!");
         }
     }
 }
